ad63b0e6840578f2b1c193b0f282ffe9377e431e
LiochaWeigel <lweigel@enssat.fr>
2018-03-23 12:07:29 +0100

    exp.js

--- exp.js
+++ exp.js
@@ -1,16 +1,16 @@
-var http = require(’http’);
+var http = require('http');
+
 http.createServer(function (req, res) {
-if (req.url == ’/’) { //ici, on manipule l’objet requ^ete
-// Home
-res.writeHead(200, {’Content-Type’: ’text/html’});
-res.end(’<a href="/hello">Say hello</a>’);
-} else if (req.url == ’/hello’) {
-// Coucou gamin
-res.writeHead(200, {’Content-Type’: ’text/html’});
-res.end(’Coucou gamin.’);
-} else {
-// Autres: 404
-res.writeHead(404);
-res.end();
-}
+	if (req.url == '/') { 
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('<a href="/hello">Say hello</a>');
+	} 
+	else if (req.url == '/hello') {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('Coucou gamin.');
+	} 
+	else {
+		res.writeHead(404);
+		res.end();
+	}
 }).listen(8080, undefined);
---
+++ exp2.js
@@ -0,0 +1,28 @@
+var express = require(’express’);
+var app = express();
+app.get(’/’, function(req, res) {
+res.setHeader(’Content-Type’, ’text/plain’);
+res.end(’Vous ^etes `a l\’accueil, que puis-je pour vous ?’);
+});
+app.get(’/livres’, function(req, res) {
+res.setHeader(’Content-Type’, ’text/plain’);
+res.end(’Vous ^etes dans la page des livres’);
+});
+app.get(’/livre/1’, function(req, res) {
+res.setHeader(’Content-Type’, ’text/plain’);
+res.end(’Vous ^etes dans la page du livre 1’);
+});
+app.get(’/ab?cd’, function(req, res) {//Ce chemin de routage fait correspondre acd et abcd.
+res.send(’ab?cd’);
+});
+app.get(’/ab+cd’, function(req, res) {//Ce chemin de routage fait correspondre abcd, abbcd, abbbcd, etc.
+res.send(’ab+cd’);
+});
+app.get(’/ab*cd’, function(req, res) {//Ce chemin de routage fait correspondre abcd, abxcd, abRABDOMcd,...
+res.send(’ab*cd’);
+});
+app.get(’/ab(cd)?e’, function(req, res) {//Ce chemin de routage fait correspondre /abe et /abcde.
+2
+res.send(’ab(cd)?e’);
+});
+app.listen(8080);
